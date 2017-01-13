d597bffa0ea50696d993d18f5d94d706b54de58a
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-01-13 09:14:19 -0800

    Set up the base javascript needed to go through the survey properly detailed javascript not added, html and css still blank

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,44 @@ var b;
 var c;
 var d;
 
-if(stack === "front"){
-
-
-} else if (stack === "back"){
-
-
-}
+$(document).ready(function(){
+  $('#stack').submit(function(event){
+    if(stack === "front") {
+      $('#front').show();
+    } else if (stack === "back") {
+      $('#back').show();
+    }
+  });
+  //end of first choice----------------------------------
+  $('#firstF').submit(function(event){
+    if(a === 1){
+      $('#css').show();
+    } else if (a === 2){
+      $('#B').show();
+    }
+  })
+  //end of second choice --------------------------------
+  $('#secondF').submit(function(event){
+    if (b === 1){
+      $('#ruby').show();
+    } else if (b === 2){
+      $('#php').show();
+    }
+  })
+  //end of third choice ----------------------------------
+  $('#firstB').submit(function(event){
+    if(c === 1){
+      $('#java').show();
+    } else if (c === 2){
+      $('#D').show();
+    }
+  })
+  //end of fourth choice ---------------------------------
+  $('#secondB').submit(function(event){
+    if(d === 1){
+      $('#java').show();
+    } else if (d === 2){
+      $('#csharp').show();
+    }
+  })
+})
